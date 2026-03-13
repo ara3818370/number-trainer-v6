5bc9465ad543a0ff145778a040c97fd265d0cfa6
ara3818370 <ara@my.portal.ai>
2026-03-13 19:43:27 +0000

    Bump SW nlt-v6i

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // sw.js — Service Worker: Cache First with Network Fallback
 // Number Trainer v6 "Jony Ive Edition"
 
-const CACHE_VERSION = 'nlt-v6h';
+const CACHE_VERSION = 'nlt-v6i';
 const ASSETS = [
   './',
   './index.html',
